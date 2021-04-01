13cf6df3eaaf60b2b9e48c530d02cae62a6c191e
Sandoque <acsandoque@gmail.com>
2021-03-31 22:22:17 -0300

    Intro javascript

---
+++ INTRO_JAVASCRIPT/js/main.js
@@ -0,0 +1,19 @@
+// Intro variáveis e strings
+
+/*var nome = "Sandoque";
+var idade = 42;
+var idade2 =  41;
+var frase = "Islândia é o Melhor País do Mundo"
+//alert(idade2 + idade) 
+//alert ("Bem vindo " + nome)
+
+console.log(idade)
+console.log(nome)
+console.log(frase.replace("Islândia", "Noruega"))
+console.log(frase.toLowerCase())
+//alert(frase.replace("islândia", "Suécia"))
+*/
+
+// Array e dicionário
+
+var lista = ["maçã", "pêra", "laranja"]
